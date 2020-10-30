1ecc893acc1c2a3c3389197e92c648ebf1caac3a
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-10-30 11:13:31 -0400

    Add files via upload

---
+++ TesterTri.java
@@ -0,0 +1,8 @@
+public class TesterTri {
+  public static void main(String[] args) {
+    Point myPoint = new Point(1.0, 2.0);
+
+    System.out.println(myPoint.getX() == 1.0);
+    System.out.println(myPoint.getY() == 2.0);
+  }
+}
